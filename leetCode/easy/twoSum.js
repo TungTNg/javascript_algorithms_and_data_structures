2b12001efd79de149d875b305180112f48717942
Tung Nguyen <mecer80@gmail.com>
2019-03-30 00:39:48 +0000

    Added some more code challenges

---
+++ leetCode/easy/twoSum.js
@@ -0,0 +1,66 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+// Approach 1:
+// var twoSum = function(nums, target) {
+//     let returnArray = [];
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = 0; j < nums.length; j++) {
+//             if(i !== j) {
+//                 if(nums[i] + nums[j] == target) {
+//                     returnArray.push(i);
+//                     returnArray.push(j);
+//                     return returnArray;
+//                 }
+//             }
+//         }
+//     }
+// }; 
+
+
+// let testArray = [3,2,4];
+// console.log(twoSum(testArray, 6));
+
+// Approach 2:
+// function twoSum(arr, target) {
+//     let lookupObj = {};
+//     for(let i = 0; i < arr.length; i++) {
+//         if(lookupObj[target - arr[i]]) return [parseInt(lookupObj[target - arr[i]]), parseInt(i)];
+//         lookupObj[arr[i]] = i.toString();
+//         // console.log(lookupObj);
+//     }
+// }
+
+// let testArray = [3,2,4];
+// console.log(twoSum(testArray, 6));
+
+// let testArray = [2,7,11,15];
+// console.log(twoSum(testArray, 9));
+
+const twoSum = (nums, target) => {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+
+    if (another in map) {
+      return [map[another], i];
+    }
+
+    map[nums[i]] = i;
+    console.log(map);
+  }
+
+  return null;
+};
+
+let testArray = [2,7,11,15];
+console.log(twoSum(testArray, 9));
