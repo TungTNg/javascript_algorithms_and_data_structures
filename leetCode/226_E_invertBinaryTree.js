1bda07bc0a2c7cf665f0b360decfb5c92ecf4acc
Tung Nguyen <mecer80@gmail.com>
2020-05-11 20:25:19 +0000

    Added Selection Sort

--- leetCode/226_E_invertBinaryTree.js
+++ leetCode/226_E_invertBinaryTree.js
@@ -1,3 +1,24 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+         4
+       /   \
+      2     7
+     / \   / \
+    1   3 6   9
+Output:
+
+         4
+       /   \
+      7     2
+     / \   / \
+    9   6 3   1
+*/
+
 function TreeNode(val, left, right) {
     this.val = val
     this.left = left
---
+++ sortingAlgorithms/selectionSort.js
@@ -0,0 +1,27 @@
+// Store the first element as the smallest value you've seen so far
+// Compare this item to the next item in the array until you find a smaller number
+// If the smaller number is found, designate that smaller number to be the new "minimum" and continue until the end of the array.
+// If the "minimum" is not the value (index) you initially began with, swap the two values.
+// Repeat this with the next element until the array is sorted.
+
+// Time Complexity: O(n^2)
+
+function selectionSort(arr) {
+    for(var i = 0; i < arr.length - 1; i++) {
+        var lowest = i;
+        for(var j = i + 1; j  < arr.length; j++) {
+            if(arr[j] < arr[lowest]) {
+                lowest = j;
+            }
+        }
+        if(i !== lowest) {
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    
+    return arr;
+}
+
+console.log(selectionSort([34, 22, 10, 5, 0, 19, 17]));
